e8167b46fd1bd3f34b5a8ab49adc596352410b69
mo-debo <eldabawy_83@hotmail.com>
2017-08-20 13:52:45 +0200

    1

--- src/android/FakeLocation.java
+++ src/android/FakeLocation.java
@@ -5,25 +5,17 @@ import org.apache.cordova.CordovaPlugin;
 import org.json.JSONArray;
 import org.json.JSONException;
 import android.provider.Settings.Secure;
-import android.os.Build;
 
 public class FakeLocation extends CordovaPlugin {
 
     @Override
     public boolean execute(String action, JSONArray data, CallbackContext callbackContext) throws JSONException {
         if (action.equals("check")) {
-if(Build.VERSION.SDK_INT < 18)
-{
 			if (Secure.getString(this.cordova.getActivity().getContentResolver(), Secure.ALLOW_MOCK_LOCATION).equals("0")) {
 				callbackContext.success(0);
 			} else {
 				callbackContext.success(1);
 			}
-}else
-{
-
-
-}
             return true;
         }
 		return false;
