662773f347ea1fd77d9f508112f86e3ab9227352
mo-debo <eldabawy_83@hotmail.com>
2017-08-20 17:24:05 +0200

    new Test

--- src/android/FakeLocation.java
+++ src/android/FakeLocation.java
@@ -5,19 +5,26 @@ import org.apache.cordova.CordovaPlugin;
 import org.json.JSONArray;
 import org.json.JSONException;
 import android.provider.Settings.Secure;
-
+import Emu.EmulatorDetector;
 public class FakeLocation extends CordovaPlugin {
 
     @Override
     public boolean execute(String action, JSONArray data, CallbackContext callbackContext) throws JSONException {
         if (action.equals("check")) {
-			if (Secure.getString(this.cordova.getActivity().getContentResolver(), Secure.ALLOW_MOCK_LOCATION).equals("0")) {
-				callbackContext.success(0);
-			} else {
-				callbackContext.success(1);
-			}
+      if (Secure.getString(this.cordova.getActivity().getContentResolver(), Secure.ALLOW_MOCK_LOCATION).equals("0")) {
+        callbackContext.success(0);
+      } else {
+        callbackContext.success(1);
+      }
             return true;
-        }
-		return false;
+        }else  if (action.equals("checkEmu")) {
+		if (!EmulatorDetector.isEmulator())
+		{
+		callbackContext.success(0);
+		}else {callbackContext.success(1);}
+
+		return true;
+		}
+    return false;
     }
 }
--- www/FakeLocation.js
+++ www/FakeLocation.js
@@ -1,7 +1,10 @@
 var fakeLocation = {
   check: function (successCallback, errorCallback) {
     cordova.exec(successCallback, errorCallback, 'FakeLocation', 'check', []);
-  }
+  },
+  checkEmu: function (successCallback, errorCallback) {
+    cordova.exec(successCallback, errorCallback, 'FakeLocation', 'checkEmu', []);
+}
 }
 
 if (!window.plugins) { window.plugins = {}; }
