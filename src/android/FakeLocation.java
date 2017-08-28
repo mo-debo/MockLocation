51eea8ee7f8c2359e8854f7e030e674e9c0f561b
mo-debo <eldabawy_83@hotmail.com>
2017-08-28 12:08:11 +0200

    1

--- src/android/FakeLocation.java
+++ src/android/FakeLocation.java
@@ -6,16 +6,19 @@ import org.json.JSONArray;
 import org.json.JSONException;
 import android.provider.Settings.Secure;
 import emulator.EmulatorDetector;
+import android.os.Build;
 public class FakeLocation extends CordovaPlugin {
 
     @Override
     public boolean execute(String action, JSONArray data, CallbackContext callbackContext) throws JSONException {
         if (action.equals("check")) {
+		if (Build.VERSION.SDK_INT < 18) {
       if (Secure.getString(this.cordova.getActivity().getContentResolver(), Secure.ALLOW_MOCK_LOCATION).equals("0")) {
         callbackContext.success(0);
       } else {
         callbackContext.success(1);
       }
+	  }
             return true;
         }else  if (action.equals("checkEmu")) {
 		boolean isVirtual= EmulatorDetector.isEmulator();
